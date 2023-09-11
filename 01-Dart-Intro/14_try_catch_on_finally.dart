4b06cf6a0b5fe6f1bfd89615378b43897e4cb691
mikecode <miguelenrique1805@gmail.com>
2023-09-11 13:31:47 -0500

    solved warnings

--- 01-Dart-Intro/14_try_catch_on_finally.dart
+++ 01-Dart-Intro/14_try_catch_on_finally.dart
@@ -1,23 +1,22 @@
 void main() async {
   print('Init app');
-  
+
   try {
-    final response = await httpGet('http.google.pe');    
+    final response = await httpGet('http.google.pe');
     print(response);
   } on Exception catch (error) {
     print('We have a Exception: $error');
   } catch (error) {
     print(error);
   } finally {
-    print('End catch finally');    
+    print('End catch finally');
   }
   print('End app');
 }
 
-Future<String> httpGet( String url ) async {
-  
-  await Future.delayed( const Duration(seconds: 1));
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 1));
   throw Exception('No hay internet');
-  throw 'Error not response';
-    return 'Response http';
+  // throw 'Error not response';
+  //   return 'Response http';
 }
